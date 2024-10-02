e9af34dc146451f56c7b978ec743940d72e639cf
mfranck340 <franck.yaguargos@edu.uah.es>
2024-10-02 12:12:52 +0200

    modified .h

--- files/get_next_line.h
+++ files/get_next_line.h
@@ -25,6 +25,6 @@ char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char *s1, char *s2);
 
 #endif
